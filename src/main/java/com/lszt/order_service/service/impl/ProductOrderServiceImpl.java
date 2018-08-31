c50a76e1c913b937c16e35cf00702982f469e48d
xc <451597529@qq.com>
2018-08-31 17:31:09 +0800

    2018/8/30修改

--- src/main/java/com/lszt/order_service/service/impl/ProductOrderServiceImpl.java
+++ src/main/java/com/lszt/order_service/service/impl/ProductOrderServiceImpl.java
@@ -8,9 +8,7 @@ import com.lszt.order_service.service.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.Date;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -38,15 +36,20 @@ public class ProductOrderServiceImpl implements ProductOrderService {
     //feign方式
     @Override
     public ProductOrder save(int userId, int productId) {
+        //调用订单服务
         String response = productClient.findById(productId);
+        //调用用户服务，主要是获取用户名称，用户的级别或者积分信息
+        //TODO
+
+
+
         JsonNode jsonNode = JsonUtils.str2JsonNode(response);
-        System.out.println(jsonNode);
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCreateTime(new Date());
         productOrder.setUserId(userId);
         productOrder.setTradeNo(UUID.randomUUID().toString());
-        //productOrder.setProductName(productMap.get("name").toString());
-        //productOrder.setPrice(Integer.parseInt(productMap.get("price").toString()));
+        productOrder.setProductName(jsonNode.get("name").toString());
+        productOrder.setPrice(Integer.parseInt(jsonNode.get("price").toString()));
         return productOrder;
     }
 }
