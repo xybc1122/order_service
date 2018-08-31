43d0c911205b4a369f9751acc15a966e61640d8f
xc <451597529@qq.com>
2018-08-31 17:38:18 +0800

    2018/8/31修改

--- src/main/java/com/lszt/order_service/OrderServiceApplication.java
+++ src/main/java/com/lszt/order_service/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.lszt.order_service;
+﻿package com.lszt.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+//练习feign
 @SpringBootApplication
 @EnableFeignClients //Feign注解
 public class OrderServiceApplication {
